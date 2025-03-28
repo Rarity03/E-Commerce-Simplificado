9c292df9414fa4af4ce664ce8205cd1abac5e7a4
Rarity03 <kennethkael@gmail.com>
2025-03-27 22:14:29 -0600

    Reajustes Front

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // Extrae el host de la URL de la variable de entorno
-const apiUrl = import.meta.env.VITE_API_URL ? new URL(import.meta.env.VITE_API_URL).hostname : 'localhost';
+//const apiUrl = import.meta.env.VITE_API_URL ? new URL(import.meta.env.VITE_API_URL).hostname : 'localhost';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
       "localhost:5173",
-      apiUrl 
+      "e-commerce-simplificado.onrender.com"
     ],
     
   }
